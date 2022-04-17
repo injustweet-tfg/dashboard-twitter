52dc8dd920deb17fe3f3fa2fd482a1858d2d4361
angrui02 <angrui02@ucm.es>
2022-04-17 17:03:52 +0200

    routes

--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,20 @@ import DashboardLayout from './layouts/dashboard';
 //
 import DashboardApp from './pages/DashboardApp';
 import InfoApp from './pages/InfoApp';
+import { TweetsProvider } from './context';
 // ----------------------------------------------------------------------
 
 export default function Router() {
   return useRoutes([
-    { 
+    {
       element: <DashboardLayout />,
       children: [
         { path: '/', element: <Navigate to="/dashboard" /> },
-        { path: '/dashboard', element: <DashboardApp /> },
+        {
+          path: '/dashboard', element: <TweetsProvider>
+            <DashboardApp />
+          </TweetsProvider>
+        },
         { path: '/info', element: <InfoApp /> }
       ]
     }
