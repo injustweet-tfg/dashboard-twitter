9bf9af3858f6d554d7164bab6ccba358d548e321
jjavimu <jmulero@ucm.es>
2022-04-28 16:56:02 +0200

    deleted overrides

--- src/theme/overrides/index.js
+++ src/theme/overrides/index.js
@@ -1,14 +1,10 @@
 import { merge } from 'lodash';
 import Card from './Card';
 import Lists from './Lists';
-import Paper from './Paper';
 import Input from './Input';
 import Button from './Button';
-import Tooltip from './Tooltip';
-import Backdrop from './Backdrop';
 import Typography from './Typography';
 import IconButton from './IconButton';
-import Autocomplete from './Autocomplete';
 
 // ----------------------------------------------------------------------
 
@@ -16,13 +12,9 @@ export default function ComponentsOverrides(theme) {
   return merge(
     Card(theme),
     Lists(theme),
-    Paper(theme),
     Input(theme),
     Button(theme),
-    Tooltip(theme),
-    Backdrop(theme),
     Typography(theme),
     IconButton(theme),
-    Autocomplete(theme)
   );
 }
